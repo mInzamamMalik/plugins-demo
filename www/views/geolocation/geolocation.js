1e62309bf9a6c41165421bd51fc5227ca376f1fc
SkyNetServer <malikasinger1@gmail.com>
2015-09-24 16:35:32 -0800

    geolocation page ok

--- www/views/geolocation/geolocation.js
+++ www/views/geolocation/geolocation.js
@@ -3,7 +3,20 @@
  */
 angular.module("starter")
 
-  .controller("geolocationController",function($scope,$cordovaDevice) {
+  .controller("geolocationController",function($scope,$cordovaGeolocation) {
 
+    var posOptions = {
+      timeout: 10000,
+      enableHighAccuracy: false
+    };
 
+      $cordovaGeolocation.getCurrentPosition(posOptions)
+        .then(function (position) {
+          $scope. lat  = position.coords.latitude;
+          $scope. long = position.coords.longitude;
+
+          alert(position.coords.latitude + " -- " + position.coords.longitude);
+        }, function(err) {
+          console.log( err);
+        });
   });
